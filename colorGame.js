9a5b0617b9bbc52e3d7ae223b4f6456749450301
sasankdas <alladisasankdas@gmail.com>
2018-04-24 00:24:02 -0400

    change of all the squares by clicking the exact clicked color

--- colorGame.js
+++ colorGame.js
@@ -7,17 +7,28 @@ var colors = ["rgb(255, 0, 0)",
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
+var messageDisplay = document.querySelector("#message");
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
-colorDisplay
 for (var i=0; i<squares.length; i++){
     squares[i].style.backgroundColor = colors[i]; 
 
     squares[i].addEventListener("click", function(){
         var clickedColor =this.style.backgroundColor;
         if(clickedColor===pickedColor){
-            alert("correct");
+            messageDisplay.textContent = "Try Again";
+            messageDisplay.textContent = "Correct";
+            changedColors(clickedColor);
+        }else{
+            this.style.backgroundColor = "black";
+            messageDisplay.textContent = "Try Again";
         }
     })
 }
+function changedColors(color){
+    for(var i=0; i<squares.length;i++){
+squares[i].style.backgroundColor = color;
+    }
+
+    }
